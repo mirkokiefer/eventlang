488a750a37719b567ee282dba413a90a18dcf592
Mirko Kiefer <mail@mirkokiefer.com>
2012-04-22 22:58:34 +0200

    Initial Commit

---
+++ FunC/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  FunC
+//
+//  Created by Mirko Kiefer on 4/22/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+  // insert code here...
+  printf("Hello, World!\n");
+    return 0;
+}
+
