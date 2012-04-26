ccb8f3bd00d377f2eea01616b703cb2596d5926d
Mirko Kiefer <mail@mirkokiefer.com>
2012-04-26 13:11:13 +0200

    minor

--- fc_function.h
+++ fc_function.h
@@ -5,7 +5,7 @@
 #include "fun_c.h"
 
 fc_funct_tree fc_funct_tree_leaf(int param);
-fc_funct_tree fc_funct_tree_node(FCFunction function, fc_funct_tree *parameters);
+fc_funct_tree fc_funct_tree_node(FCFunction function, fc_funct_tree *arguments);
 FCFunction FCFunctionNew(FCFunction parent, int args[], int local_vars[], fc_funct_tree *local_trees);
 FCFunction FCFunctionNewPrimitive(fc_function_primitive c_fun);
 void FCFunctionInvoke(FCFunction funct, fc_value *parameters[], fc_value *buffer);
